0ab9f43800f54ad6d8ae7f8e50595f061d8c05d2
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2024-10-06 16:33:42 +0800

    Update 51代理.py

--- 51代理.py
+++ 51代理.py
@@ -1,10 +1,16 @@
+import os
+
 import requests
 import time
 import random
 from datetime import datetime, timedelta
 
+ip51_cookie = os.environ.get('51ip_cookie')
+ip51_key = os.environ.get('51ip_key')
+pushplus_token = os.environ.get('pg_pushplus_token')
 heartbeat_url = 'https://m.51daili.com/wap/user/index.html'
 signin_url = 'https://m.51daili.com/wap/user/signin.html'
+sech=f"https://aapi.51daili.com/userapi?appkey={ip51_key}"
 
 headers = {
     'Host': 'm.51daili.com',
@@ -18,48 +24,49 @@ headers = {
     'Connection': 'keep-alive',
     'Referer': 'https://m.51daili.com/wap/user/index.html',
     'Content-Length': '2',
-    'Cookie': '',
+    'Cookie': ip51_cookie
 }
 
 data = {}
+
 
 def send_heartbeat():
     try:
         response = requests.post(heartbeat_url, json=data, headers=headers)
         print(f"{datetime.now()} - Status Code: {response.status_code}")
+        print("保活成功")
     except requests.RequestException as e:
         print(f"{datetime.now()} - Error")
+
 
 def send_signin():
     try:
         response = requests.post(signin_url, json=data, headers=headers)
         if response.status_code == 200:
-            print(response.json())
+            # print(response.json())
+            print("签到完成")
         else:
             print(f"{datetime.now()} - Error")
     except requests.RequestException as e:
         print(f"{datetime.now()} - Error")
 
-def get_random_time():
-    now = datetime.now()
-    random_hour = random.randint(0, 23)
-    random_minute = random.randint(0, 59)
-    random_second = random.randint(0, 59)
-    random_time = now.replace(hour=random_hour, minute=random_minute, second=random_second)
-    if random_time < now:
-        random_time += timedelta(days=1)
-    return random_time
+def send_seh():
+    try:
+        response = requests.get(sech, headers=headers)
+        if response.status_code == 200:
+            data1=response.json()['data']['currentScore']
+            print(f"当前积分{data1}")
+        else:
+            print(f"{datetime.now()} - Error")
+    except requests.RequestException as e:
+        print(f"{datetime.now()} - Error")
 
-next_random_run_time = get_random_time()
-heartbeat_interval = 25 * 60
 
-while True:
-    now = datetime.now()
-    
-    if now >= next_random_run_time:
-        send_signin()
-        next_random_run_time = get_random_time()
-    
+now = datetime.now()
+# 判断是否在 16:30 到 16:50 之间
+if now.hour == 10 and 30 <= now.minute <= 50:
+    send_signin()
+    time.sleep(3)
+    send_seh()
+else:
     send_heartbeat()
-    
-    time.sleep(heartbeat_interval)
